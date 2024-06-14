8453ef7acaa5f28b19cee874d9f0dffb4af6208f
tushcmd <muturidavid854@gmail.com>
2024-06-14 11:21:11 +0300

    run - print.rs

---
+++ hello/target/debug/deps/hello-008dca6205857c5b.d
@@ -0,0 +1,5 @@
+C:\Users\Tush\Desktop\projects-rust\hello\target\debug\deps\libhello-008dca6205857c5b.rmeta: src/main.rs
+
+C:\Users\Tush\Desktop\projects-rust\hello\target\debug\deps\hello-008dca6205857c5b.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-d745c841361616b8.d
@@ -0,0 +1,5 @@
+C:\Users\Tush\Desktop\projects-rust\hello\target\debug\deps\libhello-d745c841361616b8.rmeta: src/main.rs
+
+C:\Users\Tush\Desktop\projects-rust\hello\target\debug\deps\hello-d745c841361616b8.d: src/main.rs
+
+src/main.rs:
