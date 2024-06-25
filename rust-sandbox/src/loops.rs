1eeff75853f344d2f6a56916c5461be148ab0f6c
tushcmd <muturidavid854@gmail.com>
2024-06-26 02:59:51 +0300

    While Loop(fizzbuzz)

--- rust-sandbox/src/loops.rs
+++ rust-sandbox/src/loops.rs
@@ -4,12 +4,28 @@ pub fn run() {
     let mut count = 0;
 
     // Infinite Loop
-    loop {
-        count +=1;
-        println!("Number: {}", count);
+    //loop {
+    //    count +=1;
+    //    println!("Number: {}", count);
+    //
+    //    if count == 20 {
+    //        break;
+    //    }
+    //}
 
-        if count == 20 {
-            break;
+    // While Loop (FizzBuzz)
+    while count <= 1000 {
+        if count % 15 == 0 {
+            println!("fizzbuzz");
+        } else if count % 3 == 0 {
+            println!("fizz");
+        } else if count % 5 == 0 {
+            println!("buzz")
+        } else {
+            println!("{}", count);            
         }
+        
+        // Inc
+        count += 1;
     }
 }
