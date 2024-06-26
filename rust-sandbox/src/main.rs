b0e9cee4f1204ea7de4c94bd0b9c31dbfc5eca25
tushcmd <muturidavid854@gmail.com>
2024-06-26 03:24:17 +0300

    Functions

--- rust-sandbox/src/main.rs
+++ rust-sandbox/src/main.rs
@@ -6,7 +6,8 @@
 //mod arrays;
 //mod vectors;
 //mod conditionals;
-mod loops;
+//mod loops;
+mod functions;
 
 fn main() {
     //println!("Hello, world!");
@@ -18,6 +19,7 @@ fn main() {
     //arrays::run();
     //vectors::run();
     //conditionals::run();
-    loops::run();
+    //loops::run();
+    functions::run();
 
 }
